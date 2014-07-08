bac7aaa8e292fe8d2e36138e2ef9c54bf48d1ee3
Dave Tucker <dave@dtucker.co.uk>
2014-07-08 16:17:00 +0100

    Make Postgres more permissive

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
-class { 'postgresql::server': }
+class { 'postgresql::server':
+  ip_mask_deny_postgres_user => '0.0.0.0/32',
+  ip_mask_allow_all_users    => '0.0.0.0/0',
+  listen_addresses           => '*',
+  ipv4acls                   => ['host all all 0.0.0.0/0 md5']
+}
 
 postgresql::server::db { 'sonar':
   user     => 'sonar',
